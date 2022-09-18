ec6f1b1e1b5fe6d0a319a47e4166e7b1a0c10950
kaco0307 <pjelfenomeno15@gmail.com>
2022-09-18 18:59:42 -0500

    Primero

---
+++ UsuarioClass/src/usuarioclass/VideoJuego.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuarioclass;
+
+/**
+ *
+ * @author lachi
+ */
+public class VideoJuego {
+    private String Nombre;
+    private int Codigo;
+    private String Codigo_Ejemplar;
+    private String IDE;
+
+    public VideoJuego() {
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getCodigo_Ejemplar() {
+        return Codigo_Ejemplar;
+    }
+
+    public void setCodigo_Ejemplar(String Codigo_Ejemplar) {
+        this.Codigo_Ejemplar = Codigo_Ejemplar;
+    }
+
+    public String getIDE() {
+        return IDE;
+    }
+
+    public void setIDE(String IDE) {
+        this.IDE = IDE;
+    }
+
+    
+    
+    
+}
+
+
